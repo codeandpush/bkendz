cd5575446698a8c5fc87cb80a4839e88db29170a
anthony <anthony.chinwo@gmail.com>
2018-03-26 00:54:18 +0100

    - added test API end point for User

--- console.js
+++ console.js
@@ -1,27 +1,20 @@
 /**
  * Created by anthony on 28/02/2016.
  */
-var repl = require('repl')
-var replHistory = require('repl.history')
+const repl = require('repl')
+const replHistory = require('repl.history')
 // environment configuration
-var epa = require('epa')
+const epa = require('epa')
 
-// database
-var models = require('./models')
-var moment = require('moment')
+const moment = require('moment')
 
-models.sequelize.sync().then(function () {
-    let envName = process.env.NODE_ENV || "dev";
+let envName = process.env.NODE_ENV || 'dev'
 
-    // open the repl session
-    let replServer = repl.start({
-        prompt: "Rype API (" + envName + ") > "
-    });
-    replHistory(replServer, process.env.HOME + '/.node_history');
-    // attach my modules to the repl context
-    replServer.context.epa = epa;
-    replServer.context.db = models;
-    replServer.context.moment = moment;
-}).catch((error) => {
-    console.error(error)
-})
+// open the repl session
+let replServer = repl.start({prompt: `Bkendz (${envName}) > `})
+
+replHistory(replServer, process.env.HOME + '/.node_history')
+// attach my modules to the repl context
+replServer.context.epa = epa
+replServer.context.lo = require('lodash')
+replServer.context.moment = moment
