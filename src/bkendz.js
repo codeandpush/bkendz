925715254bb6106fcb3b43c3053cd40cdaab43ee
anthony <anthony.chinwo@gmail.com>
2018-05-23 23:47:03 +0100

    - implemented HTTP access control headers and CORS

--- src/bkendz.js
+++ src/bkendz.js
@@ -3,26 +3,26 @@
  */
 
 class Deferred {
-
+    
     constructor() {
         this.id = `${this.constructor._ID++}`
-
+        
         this.promise = new Promise((res, rej) => {
             this.resolve = res
             this.reject = rej
         })
     }
-
+    
     cancel() {
         this.reject('Cancelled!')
     }
-
+    
     then(...args) {
         this._promise = (this._promise || this.promise).then(...args)
         return this
     }
-
-    catch(...args){
+    
+    catch(...args) {
         this._promise = (this._promise || this.promise).catch(...args)
         return this
     }
@@ -34,10 +34,10 @@ _.merge(WebSocket.prototype, Object.create(EventEmitter.prototype))
 
 class LazyObject {
     
-    constructor(getters){
+    constructor(getters) {
         this[Symbol.for('getters')] = getters
         
-        let properties = _.transform(getters, function(result, func, key) {
+        let properties = _.transform(getters, function (result, func, key) {
             result[key] = {
                 get: func
             }
@@ -48,7 +48,7 @@ class LazyObject {
 }
 
 class Bkendz extends EventEmitter {
-
+    
     constructor() {
         super()
         this.retryCount = {api: 0, server: 0}
@@ -59,66 +59,66 @@ class Bkendz extends EventEmitter {
         this._accessToken = null
         this.debugMode = false
     }
-
+    
     connect(url, options = {}) {
         let ws = new WebSocket(url, 'echo-protocol')
         EventEmitter.call(ws)
-
+        
         this.retryCount[options.retryCount]++
-
-
+        let self = this
+        
         let name = options.retryCount
-
+        
         this[name] = ws
-
+        
         let subscriptions = ws.subscriptons = {}
-
+        
         ws.on = function (eventName, callback) {
             let topic = `/${eventName}`
             console.log(`${name}: event=${eventName}, topic=${topic}`)
-
+            
             let subscribe = `/subscribe?subject=${eventName}`
             ws.json(subscribe)
                 .then((resp) => {
                     subscriptions[resp.data.subscribed] = 0
                     console.log('subscriptions:', subscriptions)
-
+                    
                     EventEmitter.prototype.on.call(ws, eventName, callback)
                 })
         }
-
-
+        
+        
         ws.json = (topic, data) => {
             let deferred = new Deferred()
             this.deferreds[deferred.id] = deferred
-
+            
             if (!_.isPlainObject(data)) data = {}
-
+            
             data.ACCESS_TOKEN = this.accessToken
             let req = {topic: `${topic}${_.includes(topic, '?') ? '&' : '?'}uid=${deferred.id}`, data: data}
             ws.send(JSON.stringify(req))
             return deferred
         }
-
+        
         ws.onmessage = (message) => {
             let msg = JSON.parse(message.data)
             let parts = _.split(msg.topic, '?', 2)
-
+            
             let parsed = _.chain(_.last(parts))
                 .replace('?', '') // a=b454&c=dhjjh&f=g6hksdfjlksd
                 .split('&') // ["a=b454","c=dhjjh","f=g6hksdfjlksd"]
                 .map(_.partial(_.split, _, '=', 2)) // [["a","b454"],["c","dhjjh"],["f","g6hksdfjlksd"]]
                 .fromPairs() // {"a":"b454","c":"dhjjh","f":"g6hksdfjlksd"}
                 .value()
-
+            
             let deferredId = parsed.uid
             let deferred = this.deferreds[deferredId]
             let topic = _.first(parts)
-
+            
             if (_.isObject(deferred)) {
                 delete this.deferreds[deferredId]
-
-                if(msg.error) deferred.reject(new Error(`[${name}] ${topic}: ${msg.error}`))
+                
+                if (msg.error) deferred.reject(new Error(`[${name}] ${topic}: ${msg.error}`))
                 else deferred.resolve(msg)
             } else if (topic === '/subscribe' && parsed.subject in subscriptions) {
                 subscriptions[parsed.subject]++
@@ -127,26 +127,43 @@ class Bkendz extends EventEmitter {
                 console.error('no pending promise', message, topic, parsed)
             }
         }
-
+        
+        ws.http = function (url, opts = {}) {
+            if (name === 'api') {
+                url = self.apiLocation[location.protocol.slice(0, location.protocol.length - 1)] + (_.startsWith(url, '/') ? '' : '/') + url
+            }
+            return new Promise((resolve, reject) => {
+                return $.ajax({
+                    success: resolve,
+                    method: 'POST',
+                    headers: {'X-Access-Token': self.accessToken},
+                    // beforeSend: (xhr) => {
+                    //     xhr.setRequestHeader('X-ACCESS-TOKEN', self.accessToken)
+                    // },
+                    error: reject, url
+                })
+            })
+        }
+        
         return new Promise((resolve, reject) => {
             ws.onopen = () => {
                 this.retryCount[options.retryCount] = 0
                 this.emit(options.connected || 'server_connected')
                 resolve(ws)
             }
-
+            
             ws.onclose = () => {
                 this.emit(options.disconnected || 'server_disconnected')
                 reject()
             }
-
+            
             ws.onerror = (err) => {
                 console.error(err)
                 reject(err)
             }
         })
     }
-
+    
     connectToServer() {
         let url = `${location.protocol === 'https:' ? 'wss://' : 'ws://'}${location.host}`
         return this.connect(url, {
@@ -158,7 +175,7 @@ class Bkendz extends EventEmitter {
                 this.server = server
             })
     }
-
+    
     connectToApi() {
         if (!this.apiLocation) Promise.reject(Error('No API location provided!'))
         let url = this.apiLocation[location.protocol === 'http:' ? 'ws' : 'wss']
@@ -168,38 +185,38 @@ class Bkendz extends EventEmitter {
                 this.api = api
             })
     }
-
+    
     init() {
         const emitWrap = (event) => {
-
+            
             let emit = event.target.getAttribute(`data-emit-${event.type}`)
             emit = `${event.type}_${emit}`
             if (this.debugMode) console.log('[Directive] emitting:', emit)
             this.emit(emit, event)
         }
-
+        
         ['keyup', 'click', 'change'].forEach((eventType) => {
             $(document).on(eventType, `[data-emit-${eventType}]`, emitWrap)
         })
-
+        
     }
-
+    
     main() {
         return new Promise((resolve, reject) => {
-
+            
             let start = () => {
                 try {
                     this.init()
                     this.connectToServer().then(resolve)
-
-                }catch (error){
+                    
+                } catch (error) {
                     reject(error)
                 }
             }
             document.addEventListener('DOMContentLoaded', start)
         })
     }
-
+    
     resolveAccess(email, password) {
         return this.api.json('/authenticate', {email: email, username: email, password})
             .then((resp) => {
@@ -207,25 +224,25 @@ class Bkendz extends EventEmitter {
                 return resp.data.tokens
             })
     }
-
+    
     get accessToken() {
         return this._accessToken
     }
-
+    
     set accessToken(token) {
         let currToken = this._accessToken
         this._accessToken = token
         this.emit('changed_accesstoken', token, currToken)
     }
-
+    
     getTemplate(name, opts) {
         if (!_.isString(name)) throw new Error('template name must be string, found %s', name)
-
+        
         opts = opts || {}
         let reload = _.isUndefined(opts.reload) ? true : opts.reload
         let rendered = opts.rendered || {}
         let cached = this._templates[name]
-
+        
         if (reload || !_.isString(cached)) {
             return this.server.json(`/template?name=${name}`, {rendered}).then((res) => {
                 this._templates[name] = res.data
@@ -235,7 +252,13 @@ class Bkendz extends EventEmitter {
             return Promise.resolve(cached)
         }
     }
-
+    
+    http(url, opts = {}) {
+        return new Promise((resolve, reject) => {
+            return $.ajax({success: resolve, error: reject, url: url})
+        })
+    }
+    
     repeat(opts) {
         opts = opts || {}
         let name = opts.template
@@ -258,21 +281,21 @@ class Bkendz extends EventEmitter {
                 })
             })
     }
-
+    
     fetch(modelName, options) {
         return this.api.json(`/fetch?model=${modelName}`, options || {})
     }
-
+    
     set dbSchema(schema) {
         let oldAs = this._schema
         this._schema = schema
         this.emit('changed_dbschema', schema, oldAs)
     }
-
+    
     get dbSchema() {
         return this._schema
     }
-
+    
 }
 
 Bkendz.VERSION = Bkendz.version = '<%= package.version %>'
